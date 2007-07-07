3d0faa3be70f182c1a5eb8c52bb804f0edd2e7f1
igor_n <igor_n@ed50bb1f-c732-0410-ab52-d52cc5a45641>
2007-07-07 13:21:22 +0000

    sources refactoring to remove X11 dependencies

--- src/modules/pax-0.6.0/clipmask.h
+++
@@ -1,7 +0,0 @@
-#ifndef CLIPMASK_H
-#define CLIPMASK_H
-
-PyObject * PaxClipMask_Intersect(PyObject * mask1, PyObject * mask2);
-PyObject * PaxClipMask_IntersectMasks(PyObject * self, PyObject * args);
-
-#endif /* CLIPMASK_H */
--- src/modules/pax-0.6.0/paxutil.h
+++
@@ -1,7 +0,0 @@
-#ifndef PAXUTIL_H
-#define PAXUTIL_H
-
-int pax_checkshortlist(int width, PyObject *list,
-		       short **parray, int *pnitems);
-
-#endif /* PAXUTIL_H */
