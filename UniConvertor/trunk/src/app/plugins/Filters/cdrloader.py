182fc70ab271eeb2ce518458e7a927c9bf6ca07a
igor_n <igor_n@ed50bb1f-c732-0410-ab52-d52cc5a45641>
2007-11-12 02:13:14 +0000

    copyrights update

--- UniConvertor/trunk/src/app/plugins/Filters/cdrloader.py
+++ UniConvertor/trunk/src/app/plugins/Filters/cdrloader.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-# Copyright (C) 2007 by Igor E. Novikov, Valek Fillipov
+# Copyright (C) 2007 by Igor E. Novikov, Valek Filippov
 #
 # This library is covered by GNU General Public License v2.0.
 # For more info see COPYRIGHTS file in sK1 root directory.
