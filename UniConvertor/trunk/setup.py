87431ee22732df6105359197d50959ce13a5f805
igor_n <igor_n@ed50bb1f-c732-0410-ab52-d52cc5a45641>
2010-03-08 10:58:30 +0000

    version updated

--- UniConvertor/trunk/setup.py
+++ UniConvertor/trunk/setup.py
@@ -87,7 +87,7 @@ if __name__ == "__main__":
 					skmod_src+'curvemisc.c', skmod_src+'skaux.c', skmod_src+'skimage.c', ])
 			
 	setup (name = 'UniConvertor',
-			version = '1.1.4',
+			version = '1.1.5pre',
 			description = 'Universal vector graphics translator',
 			author = 'Igor E. Novikov',
 			author_email = 'igor.e.novikov@gmail.com',
