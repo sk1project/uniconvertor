3e161aff0c237d7475b8d9aee49a8856bff57a67
igor_n <igor_n@ed50bb1f-c732-0410-ab52-d52cc5a45641>
2010-06-19 18:37:03 +0000

    fixes in argument processing

--- uniconvw/src/__init__.py
+++ uniconvw/src/__init__.py
@@ -5,8 +5,39 @@
 #
 # This library is covered by GNU Library General Public License.
 # For more info see COPYRIGHTS file in root directory.
+'''
+USAGE: uniconvw [INPUT FILE]
+
+Gtk frontend for UniConvertor.
+Converts one vector graphics format to another using sK1 engine.
+sK1 Team (http://sk1project.org), copyright (C) 2010 by Igor E. Novikov
+
+ Allowed input formats:
+	 AI  - Adobe Illustrator files (postscript based)
+     CDR - CorelDRAW Graphics files (7-X3,X4 versions)
+     CDT - CorelDRAW templates files (7-X3,X4 versions)
+     CCX - Corel Compressed Exchange files
+     CMX - Corel Presentation Exchange files (CMX1 format)
+     SVG - Scalable Vector Graphics files
+     FIG - XFig files
+     CGM - Computer Graphics Metafile files
+     AFF - Draw files
+     WMF - Windows Metafile files
+     SK  - Sketch/Skencil files
+     SK1 - sK1 vector graphics files
+     PLT - HPGL for cutting plotter files
+     DXF - Autocad Drawing Exchange Format
+     DST - Design format (Tajima)
+     PES - Embroidery file format (Brother)
+     EXP - Embroidery file format (Melco)
+     PCS - Design format (Pfaff home)
+
+Example: uniconvw drawing.cdr\n
+
+'''
 
 from sk1libs.utils import system
+import os, sys, string
 
 OPTIONS=[
 ('AI - Postscript based Adobe Illustrator 5.0 format', 'ai'),
@@ -40,7 +71,24 @@ IMPORTFILETYPES=[
 ('Acorn Draw files - *.aff','*.aff'),
 ]
 
-def uniconvw_run():
+def uniconvw_run():	
+	_pkgdir = __path__[0]
+	
+	app_ver = string.strip(open(os.path.join(_pkgdir, 'VERSION')).read())
+#	 or not os.path.isfile(sys.argv[1])
+	if len(sys.argv)>2:
+		print 'Incorrect argument!\n\n'
+		print '\nuniconvw',app_ver
+		print __doc__
+		sys.exit(1)
+		
+	if len(sys.argv)==2 and sys.argv[1]=='--help':
+		print '\nuniconvw',app_ver
+		print __doc__
+		sys.exit(0)
+		
+	dir=os.path.join(_pkgdir, 'resources')
+	
 	if system.get_os_family()==system.MACOSX:	
 		pass	
 	elif system.get_os_family()==system.WINDOWS:
@@ -48,6 +96,6 @@ def uniconvw_run():
 	
 	else:	
 		from uc_gtk import UniConvw
-		
-		application = UniConvw(OPTIONS,IMPORTFILETYPES)
+		icon=os.path.join(dir,'uniconvw_icon_32.png')
+		application = UniConvw(icon,OPTIONS,IMPORTFILETYPES)
 		application.main()
