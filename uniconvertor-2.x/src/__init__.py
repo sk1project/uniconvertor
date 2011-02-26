80174792f998a89c493ce2fbb81e756bdc5bd422
igor_n <igor_n@ed50bb1f-c732-0410-ab52-d52cc5a45641>
2011-02-26 02:15:09 +0000

    refactoring according PEP-0008

--- uniconvertor-2.x/src/__init__.py
+++ uniconvertor-2.x/src/__init__.py
@@ -5,8 +5,9 @@
 # This library is covered by GNU Library General Public License.
 # For more info see COPYRIGHTS file in root directory.
 
-from uc_conf import UCConfig, dummy_translate
 import uc2
+from uc_conf import UCConfig, dummy_translate
+
 
 config = UCConfig()
 setattr(uc2, "_", dummy_translate)
