be7fed1ff5efe39bfef8e78fbf4db36dc4740f5d
igor_n <igor_n@ed50bb1f-c732-0410-ab52-d52cc5a45641>
2014-09-04 11:43:29 +0000

    convert() function refactored

--- uniconvertor/src/uniconvertor/__init__.py
+++ uniconvertor/src/uniconvertor/__init__.py
@@ -47,50 +47,56 @@ sK1 Team (http://sk1project.org), copyright (C) 2007-2009 by Igor E. Novikov
 Example: uniconvertor drawing.cdr drawing.svg\n
 
 '''
+INIT_FLAG = [False, ]
 
 import sys, os, string
 
 def init_uniconv():
-	_pkgdir = __path__[0]
-	app_dir = os.path.join(_pkgdir, 'app')
-	sys.path.insert(1, _pkgdir)
+	if not INIT_FLAG[0]:
+		sys.path.insert(1, __path__[0])
+		INIT_FLAG[0] = True
 
-def uniconv_run():	
+
+def uniconv_run():
 	_pkgdir = __path__[0]
 	app_dir = os.path.join(_pkgdir, 'app')
 	app_ver = string.strip(open(os.path.join(app_dir, 'VERSION')).read())
 
-	if len(sys.argv)<3 or sys.argv[1]=='--help':
-		print '\nUniConvertor',app_ver
+	if len(sys.argv) < 3 or sys.argv[1] == '--help':
+		print '\nUniConvertor', app_ver
 		print __doc__
 		sys.exit(0)
-		
-	options=[]
-	input_file=sys.argv[-2]
-	output_file=sys.argv[-1]
-	
-	if len(sys.argv)>3:
-		options=sys.argv[1:-2]
-		
+
+	options = []
+	input_file = sys.argv[-2]
+	output_file = sys.argv[-1]
+
+	if len(sys.argv) > 3:
+		options = sys.argv[1:-2]
+
 	if not os.path.isfile(input_file):
 		print '\nERROR: %s file is not found!' % input_file
-		print '\nUniConvertor',app_ver
+		print '\nUniConvertor', app_ver
 		print __doc__
 		sys.exit(1)
-	
-	sys.path.insert(1, _pkgdir)
-	
+
+	convert(input_file, output_file, options)
+	sys.exit(0)
+
+def convert(input_file, output_file, options=[]):
+
+	init_uniconv()
+
 	from app.io import load
 	from uniconvertor import filters
 	import app
-	
+
 	app.init_lib()
-	
+
 	filters.load_plugin_configuration()
-	
-	if len(options) and options[0]=='-parse':
+
+	if len(options) and options[0] == '-parse':
 		load.parse_drawing(input_file, output_file)
-		sys.exit(0)
 	else:
 		doc = load.load_drawing(input_file)
 		extension = os.path.splitext(output_file)[1].lower()
@@ -102,5 +108,5 @@ def uniconv_run():
 			sys.stderr.write('ERROR: unrecognized extension %s\n' % extension)
 			sys.exit(1)
 		doc.Destroy()
-	
-	sys.exit(0)
+
+
