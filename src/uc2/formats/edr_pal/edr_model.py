d558f01d177bd9e24c94cf5c5443b3503c9ef554
max <maxim.s.barabash@gmail.com>
2019-09-03 22:52:53 +0300

    refactoring

--- src/uc2/formats/edr_pal/edr_model.py
+++ src/uc2/formats/edr_pal/edr_model.py
@@ -28,9 +28,10 @@ class EDR_Palette(BinaryModelObject):
 
     def parse(self, loader):
         while True:
-            color = EDR_Color()
-            color.parse(loader)
-            if color.chunk:
+            chunk = loader.readbytes(4)
+            if chunk:
+                color = EDR_Color()
+                color.chunk = chunk
                 self.add(color)
             else:
                 break
@@ -51,12 +52,6 @@ class EDR_Color(BinaryModelObject):
     """
     hexcolor = ''
 
-    def parse(self, loader):
-        self.chunk = loader.readbytes(4)
-        if self.chunk:
-            data = struct.unpack('BBBB', self.chunk)
-            self.hexcolor = '#%02x%02x%02x' % data[:3]
-
     def update_for_sword(self):
         self.cache_fields = [
             (0, 3, 'rgb'),
@@ -64,11 +59,20 @@ class EDR_Color(BinaryModelObject):
         ]
 
     def update(self):
-        if self.hexcolor:
-            hexcolor = self.hexcolor
-            items = (hexcolor[1:3], hexcolor[3:5], hexcolor[5:], '0')
-            data = [int(x, 0x10) for x in items]
-            self.chunk = struct.pack('BBBB', *data)
+        if self.chunk:
+            self.deserialize()
+        else:
+            self.serialize()
+
+    def deserialize(self):
+        data = struct.unpack('BBBB', self.chunk)
+        self.hexcolor = '#%02x%02x%02x' % data[:3]
+
+    def serialize(self):
+        hexcolor = self.hexcolor
+        items = (hexcolor[1:3], hexcolor[3:5], hexcolor[5:], '0')
+        data = [int(x, 0x10) for x in items]
+        self.chunk = struct.pack('BBBB', *data)
 
     def resolve(self, name=''):
         return True, self.hexcolor, ''
