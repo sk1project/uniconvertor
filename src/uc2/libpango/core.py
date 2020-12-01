f787c8edc1728362dcceb23d16fcdae43de595bb
k-bronski <krzysztof.p.bronski@gmail.com>
2020-12-02 00:24:49 +0200

    copyrights fix

--- src/uc2/libpango/core.py
+++ src/uc2/libpango/core.py
@@ -1,5 +1,6 @@
 #
 #  Copyright (C) 2016-2020 by Ihor E. Novikov
+#  Copyright (C) 2020 by Krzysztof Broński
 #
 #  This program is free software: you can redistribute it and/or modify
 #  it under the terms of the GNU Affero General Public License
